9aeca1896339dc67f1fb46a574cc92b5962693f3
dgour007 <30890754+dgour007@users.noreply.github.com>
2022-07-10 17:22:37 +0530

    update 3

--- FIle Download Example Spring boot application/src/main/java/com/codewith/abhishek/DownloadController.java
+++ FIle Download Example Spring boot application/src/main/java/com/codewith/abhishek/DownloadController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
